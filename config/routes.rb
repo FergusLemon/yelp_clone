dd00bc8527ed6d60275c36e07704aafa16617150
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-25 10:07:59 +0000

    complete version 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  # You can have the root of your site routed with "root"
+  root to: 'restaurants#index'
+  
   devise_for :users
   # get 'restaurants' => 'restaurants#index'
   resources :restaurants do
@@ -9,8 +12,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root to: 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -19,8 +19,8 @@ feature "User can sign in and out:" do
       visit ('/')
       click_link 'Sign up'
       fill_in('Email', with: 'test@example.com')
-      fill_in('Password', with: 'testest')
-      fill_in('Password confirmation', with: 'testtest')
+      fill_in('Password', with: 'password')
+      fill_in('Password confirmation', with: 'password')
       click_button ('Sign up')
      end
 
